07fa56ad065026aa5ed4af9d721a1e82097c7939
Tobias Goettert <tobiasheitor@gmail.com>
2016-11-10 21:15:21 -0200

    Criação de classes de entidade

--- NetBeansProjects/transportadora/src/transportadora/model/Caminhao.java
+++ NetBeansProjects/transportadora/src/transportadora/model/Caminhao.java
@@ -1,25 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package transportadora.model;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-public class Caminhao {
-   private String modelo;
-   private double capacidade;
+/**
+ *
+ * @author aluno
+ */
+@Entity
+public class Caminhao implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private Integer capacidade;
 
-    public String getModelo() {
-        return modelo;
+    public Long getId() {
+        return id;
     }
 
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public double getCapacidade() {
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCapacidade() {
         return capacidade;
     }
 
-    public void setCapacidade(double capacidade) {
+    public void setCapacidade(Integer capacidade) {
         this.capacidade = capacidade;
     }
     
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Caminhao)) {
+            return false;
+        }
+        Caminhao other = (Caminhao) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "transportadora.model.Caminhao[ id=" + id + " ]";
+    }
     
 }
--- NetBeansProjects/transportadora/src/transportadora/model/Pedido.java
+++ NetBeansProjects/transportadora/src/transportadora/model/Pedido.java
@@ -16,13 +16,13 @@ import javax.persistence.Id;
  * @author aluno
  */
 @Entity
-public class Fornecedor implements Serializable {
+public class Pedido implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String nome;
     private String produto;
+    private Integer pesoTotal;
 
     public Long getId() {
         return id;
@@ -32,20 +32,20 @@ public class Fornecedor implements Serializable {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getProduto() {
         return produto;
     }
 
     public void setProduto(String produto) {
         this.produto = produto;
+    }
+
+    public Integer getPesoTotal() {
+        return pesoTotal;
+    }
+
+    public void setPesoTotal(Integer pesoTotal) {
+        this.pesoTotal = pesoTotal;
     }
     
     
@@ -60,10 +60,10 @@ public class Fornecedor implements Serializable {
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Fornecedor)) {
+        if (!(object instanceof Pedido)) {
             return false;
         }
-        Fornecedor other = (Fornecedor) object;
+        Pedido other = (Pedido) object;
         if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
@@ -72,7 +72,7 @@ public class Fornecedor implements Serializable {
 
     @Override
     public String toString() {
-        return "transportadora.model.Fornecedor[ id=" + id + " ]";
+        return "transportadora.model.Pedido[ id=" + id + " ]";
     }
     
 }
