a520ba0c17a95967b3207cc4f9b60dcaa577ca21
Tobias Goettert <tobiasheitor@gmail.com>
2016-11-10 20:12:28 -0200

    Criação do projeto

---
+++ NetBeansProjects/transportadora/src/transportadora/model/Caminhao.java
@@ -0,0 +1,25 @@
+package transportadora.model;
+
+
+public class Caminhao {
+   String modelo;
+   double capacidade;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(double capacidade) {
+        this.capacidade = capacidade;
+    }
+    
+    
+}
